232042734dfc78204b7b6e985d84995408eb2e68
4Furki4 <muhammedcengiz1@gmail.com>
2022-03-22 03:29:39 +0300

    Dizileri oylayacak olan DiziOyla metodu eklendi.

--- Diziler.cs
+++ Diziler.cs
@@ -2,14 +2,32 @@ namespace Oylama
 {
     public class Diziler
     {
-        private int himym;
-        private int breakingBad;
-        private int prisonBreak;
-        private int blackMirror;
+        private int himym; //1
+        private int breakingBad; //2
+        private int prisonBreak; //3 
+        private int blackMirror; //4
 
         public int HIMYM { get => himym; set => himym = value; }
         public int BreakingBad { get => breakingBad; set => breakingBad = value; }
         public int PrisonBreak { get => prisonBreak; set => prisonBreak = value; }
         public int BlackMirror { get => blackMirror; set => blackMirror = value; }
+        public void DiziOyla(int secilen)
+        {
+            switch (secilen)
+            {
+                case 1:
+                    HIMYM++;
+                    break;
+                case 2:
+                    BreakingBad++;
+                    break;
+                case 3:
+                    PrisonBreak++;
+                    break;
+                case 4:
+                    BlackMirror++;
+                    break;
+            }
+        }
     }
 }
