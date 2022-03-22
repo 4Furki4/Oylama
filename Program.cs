9357e7b9c68eb3276d29a2e14cb58f79a4b3e4da
4Furki4 <muhammedcengiz1@gmail.com>
2022-03-22 04:54:31 +0300

    Kullanıcı ismi bulunamadığında yapılacak işlemler while dönügü içerisine yazıldı.

--- Program.cs
+++ Program.cs
@@ -7,11 +7,39 @@ namespace Oylama
     {
         static void Main(string[] args)
         {   
+            Kullanıcı _kullanıcı= new Kullanıcı();
+            Konsol_Islemleri konsol= new Konsol_Islemleri();
             List<Kullanıcı> kullanıcılar = new List<Kullanıcı>(); // 4 tane varsayılan kullanıcı atadım.
             kullanıcılar.Add(new Kullanıcı("Furkan"));
             kullanıcılar.Add(new Kullanıcı("Betül"));
             kullanıcılar.Add(new Kullanıcı("Semiha"));
             kullanıcılar.Add(new Kullanıcı("Aybüke"));
+            bool devam=true;
+            while (devam)
+            {
+                System.Console.WriteLine("Lütfen kullanıcı isminizi giriniz:\n");
+                string input= Console.ReadLine().Trim();
+                foreach (var kullanıcı in kullanıcılar)
+                {
+                    if (kullanıcı.Username==input)
+                    {
+                        
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("Girmiş olduğunuz kullanıcı ismi bulunamadı.\n"+
+                        "Çıkış yapmak istiyorsanız (1), yeni kullanıcı eklemek istiyorsanız (2) yazınız.");
+                        if (Console.ReadLine()=="1")
+                        {
+                            System.Console.WriteLine("Konsol işleminden çıkılıyor, tekrar görüşmek üzere...");
+                            devam=false;
+                        }else if (Console.ReadLine()=="2")
+                        {
+                            _kullanıcı.KullanıcıKaydet(kullanıcılar, konsol.Kullanıcı_ismi_alma());
+                        }
+                    }
+                }
+            }
         }
     }
 }
