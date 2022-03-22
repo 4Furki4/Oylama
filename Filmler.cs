72e6e071fbb97a91fd8b9966d419442c6ba2a241
4Furki4 <muhammedcengiz1@gmail.com>
2022-03-22 03:31:03 +0300

    Filmleri oylayacak olan FilmOyla metodu eklendi.

--- Filmler.cs
+++ Filmler.cs
@@ -11,5 +11,24 @@ namespace Oylama
         public int GreenBook { get => greenBook; set => greenBook = value; }
         public int DriveMyCar { get => driveMyCar; set => driveMyCar = value; }
         public int TheLordOfTheRings { get => theLordOfTheRings; set => theLordOfTheRings = value; }
+
+        public void FilmOyla(int secilen)
+        {
+            switch (secilen)
+            {
+                case 1:
+                    TheBatman++;
+                    break;
+                case 2:
+                    GreenBook++;
+                    break;
+                case 3:
+                    DriveMyCar++;
+                    break;
+                case 4:
+                    TheLordOfTheRings++;
+                    break;
+            }
+        }
     }
 }
