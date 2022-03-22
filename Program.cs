8ee1f2f47703a489749d6225ce2edbcbd1282674
4Furki4 <muhammedcengiz1@gmail.com>
2022-03-22 03:00:35 +0300

    Kullanıcı tipinde liste oluşturdum ve listeye varsayılan olarak 4 kişi ekledim

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Oylama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            List<Kullanıcı> kullanıcılar = new List<Kullanıcı>(); // 4 tane varsayılan kullanıcı atadım.
+            kullanıcılar.Add(new Kullanıcı("Furkan"));
+            kullanıcılar.Add(new Kullanıcı("Betül"));
+            kullanıcılar.Add(new Kullanıcı("Semiha"));
+            kullanıcılar.Add(new Kullanıcı("Aybüke"));
+        }
+    }
+}
