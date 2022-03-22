08c0f723e7b8a7c3f7d9243cfd7d48639ab74db9
4Furki4 <muhammedcengiz1@gmail.com>
2022-03-22 03:14:48 +0300

    Diziler sınıfına propları eklendi.

---
+++ Diziler.cs
@@ -0,0 +1,15 @@
+namespace Oylama
+{
+    public class Diziler
+    {
+        private int himym;
+        private int breakingBad;
+        private int prisonBreak;
+        private int blackMirror;
+
+        public int HIMYM { get => himym; set => himym = value; }
+        public int BreakingBad { get => breakingBad; set => breakingBad = value; }
+        public int PrisonBreak { get => prisonBreak; set => prisonBreak = value; }
+        public int BlackMirror { get => blackMirror; set => blackMirror = value; }
+    }
+}
