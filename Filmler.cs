1a52549358a7edc07677b3c1d3977fa661382cd9
4Furki4 <muhammedcengiz1@gmail.com>
2022-03-22 03:09:35 +0300

    Filmler sınıfına propları eklendi.

---
+++ Filmler.cs
@@ -0,0 +1,15 @@
+namespace Oylama
+{
+    public class Filmler
+    {
+        private int theBatman;
+        private int greenBook;
+        private int driveMyCar;
+        private int theLordOfTheRings;
+
+        public int TheBatman { get => theBatman; set => theBatman = value; }
+        public int GreenBook { get => greenBook; set => greenBook = value; }
+        public int DriveMyCar { get => driveMyCar; set => driveMyCar = value; }
+        public int TheLordOfTheRings { get => theLordOfTheRings; set => theLordOfTheRings = value; }
+    }
+}
